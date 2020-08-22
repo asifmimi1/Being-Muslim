c9946fe019a5bab2c0c10a358efceb8e27971501
Asif Mimi <30404856+asifmimi1@users.noreply.github.com>
2020-08-22 23:40:49 +0600

    Initial Commit

---
+++ Being Muslim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Being Muslim
+//
+//  Created by Asif's Mac on 22/8/20.
+//  Copyright © 2020 Asif's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
